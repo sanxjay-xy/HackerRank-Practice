818aa722f1a6d59bf497a973a8900f5c8849900d
Sanjay Praveen <sp916356@gmail.com>
2025-05-21 14:34:09 +0530

    Series creation

---
+++ JavaLoops2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+// CREATE GIVEN SERIES
+public class JavaLoops2{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int sum = a;
+            for(int j=0;j<n;j++){
+                sum+=(1<<j) * b;
+                System.out.print(sum + " ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
