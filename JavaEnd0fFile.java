a88a9c8de251e8b97572d42aed7961efa7aaba02
Sanjay Praveen <sp916356@gmail.com>
2025-05-21 14:36:27 +0530

    read lines till EOF

---
+++ JavaEnd0fFile.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaEnd0fFile {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i=0;
+        while(sc.hasNext()){
+            i++;
+            System.out.println(i+" "+sc.nextLine());
+        }
+    }
+}
