d3c966b5ad67c94eb09cdc4c1f1f27a352207715
Sanjay Praveen <sp916356@gmail.com>
2025-05-21 14:24:03 +0530

    renaming file

--- WelcomeToJava.java
+++ WelcomeToJava.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class WelcomeToJava {
 
     public static void main(String[] args) {
         System.out.println("Hello, World.");
